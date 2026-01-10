6132f3c1c4ab11481b2d5288e19396b15d7a8075
Camerjunker <ruthboazandersen@gmail.com>
2026-01-10 12:18:26 +0100

    Har opdateret GameModel med reset og highscore.

--- src/main/java/snake/GameModel.java
+++ src/main/java/snake/GameModel.java
@@ -1,5 +1,33 @@
 package snake;
 
 public class GameModel {
+    // Variabel for spillers highscore
+    private int gameScore;
+
+    // Lav en GameModel object
+    public GameModel(){
+        // Reset variabler
+        reset();
+    }
+
+    // Spiller får et point
+    public void incScore(){
+        gameScore++;
+    }
+
+    // Giver nuværende highscore
+    public void getScore() {
+        return gameScore;
+    }
+
+    // Sæt highscore til 0 når spiller taber
+    public void reset(){
+        gameScore = 0;
+    }
+
+
     
+
+
+
 }
