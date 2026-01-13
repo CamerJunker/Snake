46105dc93efbe46aa8d4260e066de62808fbcc47
Camerjunker <ruthboazandersen@gmail.com>
2026-01-13 06:58:47 +0100

    Har ændret i SnakePanel, med BufferedImage og graphics2d for at slangen ikke bliver langsom!

--- src/main/java/snake/SnakePanel.java
+++ src/main/java/snake/SnakePanel.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class SnakePanel extends JPanel {
     // Laver en GameModel object
@@ -14,10 +16,16 @@ public class SnakePanel extends JPanel {
     //Celle Størrelsen
     private static final int CELL_SIZE = 10;
 
+    private int columnSize;
+    private int rowSize;
+
     public SnakePanel(GameModel model) {
+        columnSize = model.getCols() * CELL_SIZE;
+        rowSize = model.getRows() * CELL_SIZE;
+
         this.model = model;
 
-        this.setPreferredSize(new Dimension(model.getCols() * CELL_SIZE, model.getRows() * CELL_SIZE));
+        this.setPreferredSize(new Dimension(columnSize, rowSize));
         this.setBackground(DARK_GREEN);
 
         // View må ikke håndtere input, men panelet skal gerne kunne få fokus
@@ -28,11 +36,18 @@ public class SnakePanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        // Buffered Image er super vigtigt for at slangen ikke tegnes langsomt efter få sekunder!
+        BufferedImage bufferedImage = new BufferedImage(columnSize, rowSize, BufferedImage.TYPE_INT_ARGB);
+        Graphics bufferedGraphics = bufferedImage.getGraphics();
+
+        // Alt skal helst tegnes med dette!
+        Graphics2D g2d = (Graphics2D) bufferedGraphics;
+
         // tegner mad
         Cell food = model.getFood();
         if (food != null) {
-            g.setColor(Color.ORANGE);
-            g.fillRect(
+            g2d.setColor(Color.ORANGE);
+            g2d.fillRect(
                 food.c() * CELL_SIZE,
                 food.r() * CELL_SIZE,
                 CELL_SIZE,
@@ -44,19 +59,22 @@ public class SnakePanel extends JPanel {
         boolean head = true;
         for (Cell c : model.getSnake()) {
             if (head) {
-                g.setColor(Color.GREEN.darker()); // hoved
+                g2d.setColor(Color.GREEN.darker()); // hoved
                 head = false;
             } else {
-                g.setColor(Color.GREEN); // krop
+                g2d.setColor(Color.GREEN); // krop
             }
 
-            g.fillRect(
+            g2d.fillRect(
                 c.c() * CELL_SIZE,
                 c.r() * CELL_SIZE,
                 CELL_SIZE,
                 CELL_SIZE
             );
         }
+
+        // Her tegnes det endeligt!
+        g.drawImage(bufferedImage, 0, 0, this);
 
     }
 
