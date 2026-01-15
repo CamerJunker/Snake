62639ca7d0af7d065442e7be3248d393a5ff22f5
JohnTB12 <Daztezgaming@gmail.com>
2026-01-15 18:21:34 +0100

    implementeret et HUD

--- src/main/java/snake/SnakePanel.java
+++ src/main/java/snake/SnakePanel.java
@@ -3,6 +3,8 @@ package snake;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -15,17 +17,19 @@ public class SnakePanel extends JPanel {
 
     //Celle Størrelsen
     private static final int CELL_SIZE = 10;
+    private static final int HUD_HEIGHT = 24;
+    private static final int HUD_PADDING = 6;
 
-    private int columnSize;
-    private int rowSize;
+    private int boardWidth;
+    private int boardHeight;
 
     public SnakePanel(GameModel model) {
-        columnSize = model.getCols() * CELL_SIZE;
-        rowSize = model.getRows() * CELL_SIZE;
+        boardWidth = model.getCols() * CELL_SIZE;
+        boardHeight = model.getRows() * CELL_SIZE;
 
         this.model = model;
 
-        this.setPreferredSize(new Dimension(columnSize, rowSize));
+        this.setPreferredSize(new Dimension(boardWidth, boardHeight + HUD_HEIGHT));
         this.setBackground(DARK_GREEN);
 
         // View må ikke håndtere input, men panelet skal gerne kunne få fokus
@@ -37,11 +41,26 @@ public class SnakePanel extends JPanel {
         super.paintComponent(g);
 
         // Buffered Image er super vigtigt for at slangen ikke tegnes langsomt efter få sekunder!
-        BufferedImage bufferedImage = new BufferedImage(columnSize, rowSize, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage bufferedImage = new BufferedImage(boardWidth, boardHeight + HUD_HEIGHT, BufferedImage.TYPE_INT_ARGB);
         Graphics bufferedGraphics = bufferedImage.getGraphics();
 
         // Alt skal helst tegnes med dette!
         Graphics2D g2d = (Graphics2D) bufferedGraphics;
+
+        // clear background
+        g2d.setColor(DARK_GREEN);
+        g2d.fillRect(0, 0, boardWidth, boardHeight + HUD_HEIGHT);
+
+        // HUD background
+        g2d.setColor(new Color(30, 30, 30));
+        g2d.fillRect(0, 0, boardWidth, HUD_HEIGHT);
+
+        // HUD text
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(g2d.getFont().deriveFont(Font.BOLD, 12f));
+        String hudText = "Score: " + model.getScore() + "   State: " + model.getState();
+        int hudTextY = (HUD_HEIGHT + g2d.getFontMetrics().getAscent()) / 2;
+        g2d.drawString(hudText, HUD_PADDING, hudTextY);
 
         // tegner mad
         Cell food = model.getFood();
@@ -49,7 +68,7 @@ public class SnakePanel extends JPanel {
             g2d.setColor(Color.ORANGE);
             g2d.fillRect(
                 food.c() * CELL_SIZE,
-                food.r() * CELL_SIZE,
+                food.r() * CELL_SIZE + HUD_HEIGHT,
                 CELL_SIZE,
                 CELL_SIZE
             );
@@ -67,10 +86,23 @@ public class SnakePanel extends JPanel {
 
             g2d.fillRect(
                 c.c() * CELL_SIZE,
-                c.r() * CELL_SIZE,
+                c.r() * CELL_SIZE + HUD_HEIGHT,
                 CELL_SIZE,
                 CELL_SIZE
             );
+        }
+
+        if (model.isGameOver()) {
+            g2d.setColor(new Color(0, 0, 0, 150));
+            g2d.fillRect(0, HUD_HEIGHT, boardWidth, boardHeight);
+
+            g2d.setColor(Color.WHITE);
+            g2d.setFont(g2d.getFont().deriveFont(Font.BOLD, 18f));
+            String gameOver = "Game Over";
+            FontMetrics fm = g2d.getFontMetrics();
+            int textX = (boardWidth - fm.stringWidth(gameOver)) / 2;
+            int textY = HUD_HEIGHT + (boardHeight / 2) - fm.getHeight() / 2 + fm.getAscent();
+            g2d.drawString(gameOver, textX, textY);
         }
 
         // Her tegnes det endeligt!
