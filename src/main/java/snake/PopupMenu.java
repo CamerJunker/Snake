2a72b9870dbe7686eeea1fde8b07b5964fbaca30
Camerjunker <ruthboazandersen@gmail.com>
2026-01-16 00:32:29 +0100

    Lagt labels til for at vise hvad der skal skrives i inputfelterne i popupmenu.

--- src/main/java/snake/PopupMenu.java
+++ src/main/java/snake/PopupMenu.java
@@ -14,7 +14,9 @@ import javax.swing.JTextField;
 import javax.swing.JPanel;
 
 public class PopupMenu extends JFrame implements ActionListener{
-    JLabel label = new JLabel("Change Game Size");
+    JLabel ChangeGameSizelabel = new JLabel("Change Game Size");
+    JLabel rowLabel = new JLabel("Row size (5-100)");
+    JLabel colLabel = new JLabel("Column size (5-100)");
 
     JTextField rowEntry = new  JTextField();
     JTextField colEntry = new  JTextField();
@@ -45,25 +47,29 @@ public class PopupMenu extends JFrame implements ActionListener{
         this.setResizable(false);
 
         ExitButton.setFont(new Font(null,Font.PLAIN, TextSize));
-        label.setFont(new Font(null,Font.PLAIN, TextSize));
+        ChangeGameSizelabel.setFont(new Font(null,Font.PLAIN, TextSize+2));
         rowEntry.setFont(new Font(null,Font.PLAIN, TextSize));
         colEntry.setFont(new Font(null,Font.PLAIN, TextSize));
         ChangeGridSize.setFont(new Font(null,Font.PLAIN, TextSize));
+        rowLabel.setFont(new Font(null,Font.PLAIN, TextSize));
+        colLabel.setFont(new Font(null,Font.PLAIN, TextSize));
 
         this.setLayout((new FlowLayout(FlowLayout.CENTER, spacing, spacing)));
 
         this.setTitle("Menu");
 
-        entryPanel.setLayout(new GridLayout(1,2,spacing,spacing));
-        entryPanel.setPreferredSize(new Dimension(windowDimension-30,objectHeight));
+        entryPanel.setLayout(new GridLayout(2,2,spacing,spacing));
+        entryPanel.setPreferredSize(new Dimension(windowDimension-30,(objectHeight + spacing)*2));
 
         // Add actionlisteners to buttons
         ChangeGridSize.addActionListener(this);
         ExitButton.addActionListener(this);
 
         // Add objects to window
-        this.add(label);        
+        this.add(ChangeGameSizelabel);        
         this.add(entryPanel);
+        entryPanel.add(rowLabel);
+        entryPanel.add(colLabel);
         entryPanel.add(rowEntry);
         entryPanel.add(colEntry);
         this.add(ChangeGridSize);
